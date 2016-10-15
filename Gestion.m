ce3839fc074a1453bc2eacadb89f3f7814123a93
ldevillez <louis.devillez@student.uclouvain.be>
2016-10-15 19:44:30 +0200

    Correction equation double

--- Gestion.m
+++ Gestion.m
@@ -1,9 +1,12 @@
-function [ output_args ] = Gestion(FMethRefIN, FhydrRefIN Tref, TWGS )
+function [ output_args ] = Gestion(FMethRefIN, FhydrRefIN, Tref, TWGS )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
 %% Declaration et changement d'unite
+FMethRefIN = RMethRefIN * 10^6 /86400 % Conversion T/j en mol/s
+FHydrRefIN = FHydrRefIN * 10^6 /86400 % Conversion T/j en mol/s
 
+[FMethWGSHTOUT FHydrWGSHTOUT] = Double_eq(
 
 end
 
