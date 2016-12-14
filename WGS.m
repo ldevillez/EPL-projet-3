e95fca44dd076d6bc6023ada8571f0de0ced63c2
ldevillez <louis.devillez@student.uclouvain.be>
2016-12-14 18:12:37 +0100

    YOUPITOU

--- WGS.m
+++ WGS.m
@@ -1,15 +1,24 @@
-function [ XSI1 ] = WGS(a, b, c, d, T)
+function [ XSI1 ] = WGS(a, b, c, d, T,P)
 
 %Constante d'equilibre
 
-K = 10 .^(1910 ./ T - 1.764);
-P = 24;
+dH = -40;
+Cp = 2.9; %[KJ/KgK]
+
+FMassique = 1900*10^3/86400;
+
+alpha =  dH/(FMassique * Cp);
+
+K = @(T)10 .^(1910 ./ T - 1.764);
 
 %Fonction sous forme de polynome
-Pol = conv([1 c], [1 d]) - K*conv([-1 a],[-1 b]);
+fct = @(x)(c+x)(d+x) - K(T+alpha*x)* (a-x) * (b-x);
+%{
+Pol = conv([1 c], [1 d]) - K(T+alpha*)*conv([-1 a],[-1 b]);
 %Racide de ce polynome
 XSI1 = min(roots(Pol));
-
+%}
+XSI1 = fsolve(fct,20);
 
 end
 
